98a14ba23303fd8f5cb3ad03a6d25350705212b8
Jake Laurie <jake.gordon.laurie@gmail.com>
2022-07-23 16:20:53 +0800

    Make create-record read from stdin, read passkey from env

--- create-record/src/main.rs
+++ create-record/src/main.rs
@@ -1,14 +1,31 @@
 use base64::encode;
 use magic_crypt::{new_magic_crypt, MagicCryptTrait};
+use std::env;
 
 fn main() {
-    let passkey = "somesecretpassword".to_string();
+    let passkey = match env::var_os("DARKLY_PASSKEY") {
+        Some(value) => value.into_string().unwrap(),
+        None => panic!("$DARKLY_PASSKEY is not set")
+    };
 
-    encode_record(
-        "disable_ipv6".to_string(),
-        "true".to_string(),
-        passkey,
-    );
+    println!("Creating an encrypted key-value pair");
+    println!("Enter the key:");
+    let key = read();
+
+    println!("Enter the value:");
+    let value = read();
+
+    encode_record(key, value, passkey);
+}
+
+fn read() -> String {
+    let mut value = String::new();
+    std::io::stdin().read_line(&mut value).unwrap();
+    if value.ends_with('\n') {
+        value = value.trim_end().to_string();
+    }
+
+    return value;
 }
 
 // encode as base64
@@ -18,6 +35,7 @@ fn encode_record(
     value: String,
     passkey: String
 ) -> String {
+    println!("key: {} value: {}", key, value);
     let encoded_key = encode(key);
     let encoded_value = encode(value);
     let encoded = format!("{encoded_key}|{encoded_value}");
@@ -27,10 +45,9 @@ fn encode_record(
 }
 
 fn encrypt(passkey: String, value: String) -> Option<String> {
-    println!("Encrypting with key: {}, value: {}", passkey, value);
     let magic_crypt = new_magic_crypt!(passkey, 256);
     let result = magic_crypt.encrypt_str_to_base64(value);
 
-    println!("Result: {}", result);
+    println!("{}", result);
     return Some(result);
 }
